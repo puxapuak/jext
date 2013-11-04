723a1a81dcf8ef1317424b249f95c037387ffc98
puxapuak <puxapuak@gmail.com>
2013-11-04 07:46:27 -0600

    Tiny change to try to figure this shit out

--- jext/src/jext/net/client/jextmain.java
+++ jext/src/jext/net/client/jextmain.java
@@ -7,6 +7,7 @@ public class jextmain implements EntryPoint {
 
 	public void onModuleLoad() {
 		RootLayoutPanel rootLayoutPanel = RootLayoutPanel.get();
+		rootLayoutPanel.setSize("100%", "100%");
 	}
 
 }
