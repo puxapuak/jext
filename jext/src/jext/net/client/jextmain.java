fbebe22536608c61fe61e97f0aec7114d63b881c
puxapuak <puxapuak@gmail.com>
2013-11-04 07:40:42 -0600

    Initial commit

---
+++ jext/src/jext/net/client/jextmain.java
@@ -0,0 +1,12 @@
+package jext.net.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+public class jextmain implements EntryPoint {
+
+	public void onModuleLoad() {
+		RootLayoutPanel rootLayoutPanel = RootLayoutPanel.get();
+	}
+
+}
