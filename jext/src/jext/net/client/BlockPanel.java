2d7f17343031b75a36ef47fb076aeff067acb89b
puxapuak <puxapuak@gmail.com>
2013-11-05 16:09:52 -0600

    basic form of blockpanel

--- jext/src/jext/net/client/BlockPanel.java
+++ jext/src/jext/net/client/BlockPanel.java
@@ -1,10 +1,24 @@
 package jext.net.client;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.LayoutPanel;
 
 public class BlockPanel extends Composite {
 
 	public BlockPanel() {
+		
+		VerticalPanel verticalPanel = new VerticalPanel();
+		initWidget(verticalPanel);
+		
+		SimplePanel blockHeader = new SimplePanel();
+		blockHeader.setStyleName("blockheader");
+		verticalPanel.add(blockHeader);
+		blockHeader.setHeight("31px");
+		
+		LayoutPanel layoutPanel = new LayoutPanel();
+		verticalPanel.add(layoutPanel);
 	}
 
 }
